0611151704003f322e134603cfca65cb57600d44
Adnan <adnan.chowdhury@outlook.com>
2019-07-26 12:38:30 -0500

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
-import EpisodesList from './components/EpisodesList.js';
 
 export default function App() {
   return <main>
