3ab2be1d63623a4611cedc4777b155d58937ccda
Adnan <adnan.chowdhury@outlook.com>
2019-07-26 12:31:22 -0500

    Formatted, Built React Router set up, Broken down render components,

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Card, Icon, Image} from 'semantic-ui-react'
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -15,28 +15,13 @@ export default function CharacterList() {
         .catch(error => console.log('Unexpected Error: ',error))
   }, [])//Cannot add anything to the dependency array as then useEffect will perform API requests to check if characters changed and hit API limit.
   
-  return <section className='character-list grid-view'>
+  return(
+  
+    <section className='character-list grid-view'>
       
-      {characters.map((character,index) => 
-          <Card key={index}>
-          <Image src={character.image} wrapped ui={true} />
-          <Card.Content>
-            <Card.Header>{character.name}</Card.Header>
-            <Card.Meta>
-              <span className='date'>{character.species}</span>
-            </Card.Meta>
-            <Card.Description>
-              Status:{character.status}
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <p>
-              <Icon name='location arrow' />
-              Location: {character.location.name}
-            </p>
-          </Card.Content>
-        </Card>
-        )}
+      {characters.map((character,index) => <CharacterCard key={index} character={character}/>)}
+    
     </section>
+  )
 
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,34 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card } from 'semantic-ui-react'
+
+import LocationCard from './LocationCard';
 
 export default function LocationsList() {
   // TODO: Add useState to track data from useEffect
   //locations will be stored in an array 
-  const[locations, setLocations]=useState([])
+  const [locations, setLocations] = useState([])
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
-        .get(`https://rickandmortyapi.com/api/location/`)
-        .then(response => {setLocations(response.data.results)
-          console.log(response.data.results);} )
-        .catch(error => console.log('Unexpected Error: ',error))
+      .get(`https://rickandmortyapi.com/api/location/`)
+      .then(response => { setLocations(response.data.results) })
+      .catch(error => console.log('Unexpected Error: ', error))
   }, [])//Cannot add anything to the dependency array as then useEffect will perform API requests to check if locations changed and hit API limit.
-  
-  return <section className="character-list grid view">
-      {locations.map((location,index)=>
-                  <Card key={index}>
-                  <Card.Content>
-                    <Card.Header>{location.name}</Card.Header>
-                    <Card.Meta>
-                      <span className='date'>Type: {location.type}</span>
-                    </Card.Meta>
-                    <Card.Description>
-                      Dimension:{location.dimension}
-                    </Card.Description>
-                  </Card.Content>
-                </Card>
-        )}
-      </section>
+
+  return <section className="location-list grid view">
+    {locations.map((location, index) => <LocationCard key={index} location={location} />)}
+  </section>
 }
