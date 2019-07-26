81f9077ca3e45cfed80e0a79ac728c81b188f31c
Adnan <adnan.chowdhury@outlook.com>
2019-07-26 11:50:10 -0500

    Built three page components, Pulls data and renders,

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {Card, Icon, Image} from 'semantic-ui-react'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -10,14 +11,32 @@ export default function CharacterList() {
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
         .get(`https://rickandmortyapi.com/api/character/`)
-        .then(response => {setCharacters(response.data.results)
-          console.log(response.data.results);} )
+        .then(response => {setCharacters(response.data.results)})
         .catch(error => console.log('Unexpected Error: ',error))
   }, [])//Cannot add anything to the dependency array as then useEffect will perform API requests to check if characters changed and hit API limit.
   
   return <section className='character-list grid-view'>
       
-      {characters.map(character=>character.name)}
+      {characters.map((character,index) => 
+          <Card key={index}>
+          <Image src={character.image} wrapped ui={true} />
+          <Card.Content>
+            <Card.Header>{character.name}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{character.species}</span>
+            </Card.Meta>
+            <Card.Description>
+              Status:{character.status}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <p>
+              <Icon name='location arrow' />
+              Location: {character.location.name}
+            </p>
+          </Card.Content>
+        </Card>
+        )}
     </section>
 
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card } from 'semantic-ui-react'
 
 export default function LocationsList() {
-
+  // TODO: Add useState to track data from useEffect
+  //locations will be stored in an array 
+  const[locations, setLocations]=useState([])
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+        .get(`https://rickandmortyapi.com/api/location/`)
+        .then(response => {setLocations(response.data.results)
+          console.log(response.data.results);} )
+        .catch(error => console.log('Unexpected Error: ',error))
+  }, [])//Cannot add anything to the dependency array as then useEffect will perform API requests to check if locations changed and hit API limit.
+  
+  return <section className="character-list grid view">
+      {locations.map((location,index)=>
+                  <Card key={index}>
+                  <Card.Content>
+                    <Card.Header>{location.name}</Card.Header>
+                    <Card.Meta>
+                      <span className='date'>Type: {location.type}</span>
+                    </Card.Meta>
+                    <Card.Description>
+                      Dimension:{location.dimension}
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+        )}
+      </section>
 }
